ff6d412a5477d708f0d169375184a937cdc7a8b1
zeqin <zeqin0422@gmail.com>
2021-03-23 13:26:54 +0800

    initialize project

---
+++ controller.js
@@ -0,0 +1,5 @@
+document.getElementById("show").addEventListener("click", ()=>{
+  let canvas = document.getElementById("canvas");
+  canvas.removeAttribute("hidden");
+  console.log("removed")
+}); 
---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  let canvas = createCanvas(400, 400);
+  canvas.parent('canvas')
+}
+
+function draw() {
+  background(220);
+
+}
